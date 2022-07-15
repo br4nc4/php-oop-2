e23e2f56b15b5c30bf375b0c4122fd15bd3b7f8c
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-07-15 16:28:43 +0200

    var dump paymentMethods

--- index.php
+++ index.php
@@ -5,9 +5,11 @@ require_once "classes/objects.php";
 require_once "classes/creditCard.php";
 
 $customer = new Customer();
-$creditCard = new CreditCard("visa", "74124598326597580", "445");
+$creditCard1 = new CreditCard("visa", "74124598326597580", "445");
+$creditCard2 = new CreditCard("mastercard", "7700142368575536", "915");
 
-$customer->paymentManager->add($creditCard);
+$customer->paymentManager->add($creditCard1);
+$customer->paymentManager->add($creditCard2);
 $customer->register("Marco", "Bianchi");
 
 
@@ -22,3 +24,4 @@ $customer->cart->add($products[0], $products[2], $products[1]);
 
 var_dump($customer);
 var_dump($customer->cart->getProducts());
+var_dump($customer->paymentManager->getPaymentMethods());
