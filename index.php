2dfb0f905e6eb6c01d8845eb31ff6e4ae5357043
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-07-15 15:29:16 +0200

    array products

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
 require_once "classes/customer.php";
+require_once "classes/food.php";
+require_once "classes/objects.php";
 
 $customer = new Customer();
 $customer->register("Marco", "Bianchi");
+
+$products = [
+    new Food(2.50, "crocchette pollo", "2022-10-01", ["pollo", "farina"]),
+    new Food(2.80, "crocchette salmone", "2023-01-01", ["salmone", "farina"]),
+    new Objects(10.90, "palla", "gomma", "blu"),
+    new Objects(15.50, "tiragraffi", "legno", "legno"),
+];
 
 var_dump($customer);
